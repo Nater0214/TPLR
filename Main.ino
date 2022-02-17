23867975035bffaaa454c738037b5ff5ca41dd1b
Nater0214 <gamer.nater0214@gmail.com>
2022-02-17 16:25:25 -0500

    Added Motors class and removed references to unused pin 2

--- Main.ino
+++ Main.ino
@@ -2,23 +2,22 @@
 Servo leftMotor;
 Servo rightMotor;
 
+class Motors {
+    public:
+        void forward() {
+            digitalWrite(LED_BUILTIN, HIGH);
+            leftMotor.write(0);
+            rightMotor.write(180);
+        }
+};
+Motors motors;
+
 void setup() {
     pinMode(LED_BUILTIN, OUTPUT);
-    pinMode(2, OUTPUT);
     leftMotor.attach(3);
     rightMotor.attach(5);
-}
+};
 
 void loop() {
-    digitalWrite(2, HIGH);
-
-    digitalWrite(LED_BUILTIN, HIGH);
-    leftMotor.write(0);
-    rightMotor.write(180);
-    delay(1000);
-
-    digitalWrite(LED_BUILTIN, LOW);
-    leftMotor.write(180);
-    rightMotor.write(0);
-    delay(1000);
+    motors.forward();
 }
