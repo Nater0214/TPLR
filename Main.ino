81ad472016b6f4492298784eb4d7663b4ad050a4
Nater0214 <gamer.nater0214@gmail.com>
2022-03-27 16:06:12 -0400

    Added basic distance sensor functionality

--- Main.ino
+++ Main.ino
@@ -1,6 +1,11 @@
 #include <Servo.h>
+#include <HCSR04.h>
+
 Servo leftMotor;
 Servo rightMotor;
+HCSR04 distanceSensor(9, 10);
+int readings = 0;
+float distance = 0;
 
 class Motors {
     public:
@@ -9,15 +14,47 @@ class Motors {
             leftMotor.write(0);
             rightMotor.write(180);
         }
+
+        void backwards() {
+            digitalWrite(LED_BUILTIN, HIGH);
+            leftMotor.write(180);
+            rightMotor.write(0);
+        }
+
+        void stop() {
+            digitalWrite(LED_BUILTIN, LOW);
+            leftMotor.write(90);
+            rightMotor.write(90);
+        }
+
+        void left() {
+            digitalWrite(LED_BUILTIN, HIGH);
+            leftMotor.write(0);
+            rightMotor.write(0);
+        }
 };
 Motors motors;
 
 void setup() {
+    Serial.begin(9600);
+
     pinMode(LED_BUILTIN, OUTPUT);
     leftMotor.attach(3);
     rightMotor.attach(5);
 }
 
 void loop() {
-    motors.forward();
+    distance = distanceSensor.dist();
+
+    /*
+    if (distance <= 1) {}
+    else if (distance <= 10) {
+        motors.stop();
+    } else {
+        motors.forward();
+    }
+    */
+
+    Serial.println(distance);
+    delay(500);
 }
