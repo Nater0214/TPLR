c6d1791d90cd70a18c76d1f0c0d2592ba5737f97
Nater0214 <gamer.nater0214@gmail.com>
2022-05-08 13:39:15 -0400

    Create separate file to handle motor movements and configurations

---
+++ Motors.h
@@ -0,0 +1,42 @@
+#include <Servo.h>
+
+class Motors {
+    public:
+        Servo leftMotors;
+        Servo rightMotors;
+
+        Motors(Servo left, Servo right) {
+            leftMotors = left;
+            rightMotors = right;
+        }
+
+        void forward() {
+            digitalWrite(LED_BUILTIN, HIGH);
+            leftMotors.write(0);
+            rightMotors.write(180);
+        }
+
+        void backwards() {
+            digitalWrite(LED_BUILTIN, HIGH);
+            leftMotors.write(180);
+            rightMotors.write(0);
+        }
+
+        void stop() {
+            digitalWrite(LED_BUILTIN, LOW);
+            leftMotors.write(90);
+            rightMotors.write(90);
+        }
+
+        void forwardLeft() {
+            digitalWrite(LED_BUILTIN, HIGH);
+            leftMotors.write(90);
+            rightMotors.write(180);
+        }
+
+        void forwardRight() {
+            digitalWrite(LED_BUILTIN, HIGH);
+            leftMotors.write(0);
+            rightMotors.write(90);
+        }
+};
